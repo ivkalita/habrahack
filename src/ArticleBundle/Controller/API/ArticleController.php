7abc472768d9210e6ca4e994237bf9278cadde24
Ivan Kalita <input.txt@hotmail.com>
2016-07-23 19:47:49 +1000

    Фиксы

--- src/ArticleBundle/Controller/API/ArticleController.php
+++ src/ArticleBundle/Controller/API/ArticleController.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
  * Created by IntelliJ IDEA.
  * User: Ivan Kalita
  * Date: 23.07.16
- * Time: 19:08
+ * Time: 19:08.
  */
-
 namespace ArticleBundle\Controller\API;
-
 
 use AppBundle\Classes\Payload;
 use AppBundle\Controller\BaseAPIController;
@@ -19,5 +18,4 @@ class ArticleController extends BaseAPIController implements ClassResourceInterf
     {
         return $this->response(Payload::create([]));
     }
-
 }
